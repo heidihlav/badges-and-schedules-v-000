a38b65fcc1e1ff7a7f38ece3054a2aa3afcc415c
Heidi Hlavinka <hchlav@gmail.com>
2018-03-09 15:53:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+  name = "Arel"
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.collect! do |names|
+    badges << "Hello, my name is #{names}."
+  end
+    badges
+end
+
+def assign_rooms(attendees)
+  new_array = []
+  
+  
+end
