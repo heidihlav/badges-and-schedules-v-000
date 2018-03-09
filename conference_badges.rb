c953d67fb34e3434f11c47643133da6c2f14ee5c
Heidi Hlavinka <hchlav@gmail.com>
2018-03-09 16:33:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each {|badges| puts badges}
-  assign_rooms(attendees).each {|rooms| puts rooms}
+  batch_badge_creator(attendees).each{|badge| puts badge}
+  assign_rooms(attendees).each{|rooms| puts rooms}
 end
